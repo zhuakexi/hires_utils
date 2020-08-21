09123045c2a1c19c853de80f85872220b9beb603
zhuakexi <zhuakexi@126.com>
2020-08-21 18:38:16 +0800

    python env with pipenv

--- clean_pairs.py
+++ clean_pairs.py
@@ -22,8 +22,8 @@ def clean_promiscuous(contacts:"dataframe", all_legs:dict, thread:int)->"datafra
     mask = contacts.apply(is_promiscuous, axis=1, all_legs=all_legs)
     sys.stderr.write("clean_leg: 1/%d chunk done\n"%thread)
     return contacts[~mask]
-def clean_leg_main(cell_name, num_thread, replace, out_name):
-    cell_name, num_thread, replace, out_name = args.cell_name, args.thread, args.replace_switch, args.out_name
+def clean_leg_main(args):
+    cell_name, num_thread, replace, out_name = args.filenames[0], args.thread, args.replace_switch, args.out_name
     t0 = time.time()
     #read data file
     with gzip.open(cell_name, "rt") as f:
@@ -42,15 +42,17 @@ def clean_leg_main(cell_name, num_thread, replace, out_name):
     all_legs = {key:value for key, value in all_legs.groupby("CHR")}
     sys.stderr.write("clean_leg: calculating in %.2fs\n"%(time.time()-t0))
     #multithread filtering
+    t0=time.time()
     input = np.array_split(cell, num_thread, axis=0)
     working_func = partial(clean_promiscuous,all_legs=all_legs, thread=num_thread)
-    with futures.ProcessPoolExecutor() as executor:
+    with futures.ProcessPoolExecutor(num_thread) as executor:
         res = executor.map(working_func, input)
     result = pd.concat(res,axis=0)
-
+    sys.stderr.write("clean_leg: cleaning finished in %.2fs\n"%(time.time()-t0))
     if replace == True:
         out_name = cell_name
     with gzip.open(out_name,"wt") as f:
         f.write(head)
     result.to_csv(out_name, sep="\t", \
         header=False, index=False, mode="a")
+    
