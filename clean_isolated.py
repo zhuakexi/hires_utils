e34ad913bf3eb5abaf2d9bcc19386a08782ca9c9
zhuakexi <zhuakexi@126.com>
2020-08-29 10:51:29 +0800

    using named tuple and light version funcitons to speed up

--- clean_isolated.py
+++ clean_isolated.py
@@ -15,35 +15,26 @@ def L_half(contact1, contact2):
     '''
     caculate L 0.5 norm of contact pair
     in general pandas groupby give chr1 < chr2
+    light version, assume two contacts has same chromosome order
     '''
-    if ( contact1["chr1"] == contact2["chr1"] ) and ( contact1["chr2"] == contact2["chr2"] ):
-        d_x, d_y = abs(contact1["pos1"]-contact2["pos1"]), abs(contact1["pos2"]-contact2["pos2"])
-        #print((np.math.sqrt(d_x) + np.math.sqrt(d_y))**2)
-        return (np.math.sqrt(d_x) + np.math.sqrt(d_y))**2
-    elif ( contact1["chr1"] == contact2["chr2"] ) and ( contact1["chr2"] == contact2["chr1"] ):
-        d_x, d_y = abs(contact1["pos1"]-contact2["pos2"]), abs(contact1["pos2"]-contact2["pos1"])
-        #print((np.math.sqrt(d_x) + np.math.sqrt(d_y))**2)
-        return (np.math.sqrt(d_x) + np.math.sqrt(d_y))**2
-    else:
-        #print(np.inf,contact1)
-        return np.inf
-
+    d_x, d_y = abs(contact1.pos1-contact2.pos1), abs(contact1.pos2-contact2.pos2)
+    return (np.math.sqrt(d_x) + np.math.sqrt(d_y))**2
 def is_isolate(contact, sorted_contacts:"dataframe", up_dense, up_distance)->bool:
     '''
-    #two_way_search version, calc in pos2-aixs strip
-    #check if contact is isolated, work on one chromosome pair
-    #contacts sorted by pos1 without index
-    #if two contact pos1 Eu distance > up_distance then L-0.5 distance > up_distance
+    check if contact is isolated, work on one chromosome pair
+    contacts sorted by pos1 without index
+    if two contact pos1 Eu distance > up_distance then L-0.5 distance > up_distance
+    light version
     '''
     proximity = 0
     index = bisect.bisect_right(sorted_contacts["pos1"], contact["pos1"])
-    for _, con in sorted_contacts.iloc[index-1::-1].iterrows():
-        if abs(con["pos1"]-contact["pos1"]) > up_distance:
+    for con in sorted_contacts.iloc[index-1::-1].itertuples(index=False):
+        if abs(con.pos1-contact.pos1) > up_distance or proximity >= up_dense+1:
             break
         if L_half(con,contact) < up_distance:
             proximity += 1 
-    for _, con in sorted_contacts.iloc[index:].iterrows():
-        if abs(con["pos1"]-contact["pos1"]) > up_distance:
+    for con in sorted_contacts.iloc[index:].itertuples(index=False):
+        if abs(con.pos1-contact.pos1) > up_distance or proximity >= up_dense+1:
             break
         if L_half(con,contact) < up_distance:
             proximity += 1 
@@ -67,6 +58,6 @@ def clean_isolated_main(args):
     with futures.ProcessPoolExecutor(num_thread) as executor:
         res = executor.map(working_func, input_data)
     cleaned = pd.concat(res,axis=0)
-    sys.stderr.write("clean_isolated: finished in %.2fs"%(time.time()-t0))
+    sys.stderr.write("clean_isolated: finished in %.2fs\n"%(time.time()-t0))
     write_pairs(cleaned, in_name, out_name)
     return cleaned
