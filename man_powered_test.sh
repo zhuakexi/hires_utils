35348097dec2b2724974dae5abed459e95545b0a
zhuakexi <zhuakexi@126.com>
2020-09-18 19:05:27 +0800

    using some script for testing

---
+++ man_powered_test.sh
@@ -0,0 +1,8 @@
+
+python3 hires.py script -h
+python3 hires.py script -o out -pr raw/Cell1 raw/Cell2
+python3 hires.py script -o out -pr raw/ Cell1 Cell2
+python3 hires.py script -o out -pr raw/cella.fq.gz raw/cellb.fq.gz
+python3 hires.py script -o out  raw/Cell1 raw/Cell2
+python3 hires.py script -o out  raw/ Cell1 Cell2
+python3 hires.py script -o out  raw/cell_list raw/cella.fq.gz raw/cellb.fq.gz
