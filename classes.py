ca8899f6016d390d7bf51f06d9df3cc83e56e941
zhuakexi <zhuakexi@126.com>
2020-09-23 23:10:55 +0800

    using class Data for easier using

--- classes.py
+++ classes.py
@@ -2,9 +2,15 @@ from collections import namedtuple
 from time import time
 import pandas as pd
 
-Data = namedtuple("data",["type","head","content","appendix","file"])
+#Data = namedtuple("data",["type","head","content","appendix","file"])
 Task = namedtuple("task",["data_type", "out_file", "num_thread"])
-
+class Data:
+    def __init__(self, type:str, head:str, content:str, appendix:str, file_name:str):
+        self.type = type
+        self.head = head
+        self.content = content
+        self.appendix = appendix
+        self.file_name = file_name
 class Cell:
     def __init__(self, name:str, data:Data=None, log:str=""):
         #name, data exposed
