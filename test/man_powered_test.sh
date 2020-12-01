36a80744dff0d477d6f53d17a0188e431a1e71b9
CY <zhuakexi@126.com>
2020-12-01 16:50:55 +0800

    worked test script

--- test/man_powered_test.sh
+++ test/man_powered_test.sh
@@ -19,10 +19,10 @@
 
 python3 hires.py -h
 python3 hires.py clean_leg -h
-python3 hires.py clean_leg -t 4 -o out/test.c1.pairs.gz test.pairs.gz
+python3 hires.py clean_leg -t 4 -o out/test.c1.pairs.gz test/test.pairs.gz
 python3 hires.py clean_isolated -h
-python3 hires.py clean_isolated -t 4 -o out/test.c2.pairs.gz test.pairs.gz 
+python3 hires.py clean_isolated -t 4 -o out/test.c2.pairs.gz test/test.pairs.gz 
 python3 hires.py clean_splicing -h
-python3 hires.py clean_splicing  -r bin_10K_FULL_index -o out/test.c3.pairs.gz test.pairs.gz
+python3 hires.py clean_splicing  -r bin_10K_FULL_index -o out/test.c3.pairs.gz test/test.pairs.gz
 python3 hires.py sep_clean -h
-python3 hires.py sep_clean -n 4 -i test.impute.pairs.gz -o1 out/test.hap.pairs.gz -o2 out/for_hickit.pairs.gz
+python3 hires.py sep_clean -n 4 -i test/test.impute.pairs.gz -o1 out/test.hap.pairs.gz -o2 out/for_hickit.pairs.gz
