c67ad3c7f55e66365e241719abaeabd88fc2775f
CY <zhuakexi@126.com>
2020-12-01 11:58:09 +0800

    add working module

--- sep_clean.py
+++ sep_clean.py
@@ -1,21 +1,31 @@
 import pandas as pd
 from classes import Cell, Data
+from clean_isolated import clean_isolated
+from hires_io import parse_i_pairs, write_i_pairs
 def cli(args):
-    file_name, out_name = args.file_name, args.out_name
-    cell = 
+    file_name, out_name1, out_name2, num_thread, up_dense, up_distance = \
+        args.file_name, args.out_name1, args.out_name2, args.num_thread, args.up_dense, args.up_distance
+    cell = parse_i_pairs(file_name)
+    cello1, cello2 = sep_clean(cell, num_thread, up_dense, up_distance)
+    write_i_pairs(cello1)
+    write_i_pairs(cello2)
 
 # --------- working module --------
 hap_word = {"1":"(mat)", "0":"(pat)"} #1 for maternal, 0 for paternal
 def add_ap(data:"DataFrame", row_picker:"boolean series", col_picker:"name of column", ap:"appendix"):
-    # assign value must be done in single step, don't do chained indexing
+    # add appendix for any data subset, defined by raw and col picker
+    # do inplace
+    # in pandas, assign value must be done in single step, don't do chained indexing
     ap = [ap for i in range(0, len(data.loc[row_picker, :]))] # try to figure out a more elegant way
     data.loc[row_picker, col_picker].astype("string")
     data.loc[row_picker, col_picker] = data.loc[row_picker, col_picker].str.cat(ap)
-def main(cell: Cell) -> Cell:
+def rm_hap(data:pd.DataFrame):
+    pass
+def sep_clean(cell: Cell, num_thread, up_dense, up_distance) -> Cell:
     # sep pairs haplotype and do clean isolated again
     # generate .hap.pairs for 2D analysis as well as .pairs for hickit 3d building
-    data = cell.get_data("i_pairs")
-    frame = ["content"]
+    name, data = cell.name, cell.get_data("pairs")
+    head, frame = data.head, data.content
     new_frame = pd.DataFrame()
     # blank DataFrame can concat any DataFrame
     for code in "00 01 10 11".split():
@@ -25,7 +35,7 @@ def main(cell: Cell) -> Cell:
         add_ap(frame, row_picker, "chr1", hap_word[code[0]])
         add_ap(frame, row_picker, "chr2", hap_word[code[1]])
         new_frame = pd.concat([new_frame, frame.loc[row_picker, :]])
-        #print(frame.loc[row_picker, :] )
-    #new_data = Data("i_pairs", data.head, new_frame, "hap.pairs.gz", ) 
-    cell.get_data("i_pairs").content = new_frame
-    return cell
+    cell.get_data("pairs").content = new_frame # cell seperated
+    cell_c = clean_isolated(cell, num_thread, up_dense, up_distance) # cell cleaned
+    cell_cm = Cell(name, Data(dtype="i_pairs", head=head, content=rm_hap(cell_c.get_data("pairs").content)) ) # cell mingled
+    return cell_c, cell_cm
