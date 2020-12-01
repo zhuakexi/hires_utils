7b17dbf3619a597b7bc639117f2b91c48bbb118b
CY <zhuakexi@126.com>
2020-12-01 17:33:49 +0800

    runnable sep_clean module

--- sep_clean.py
+++ sep_clean.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from classes import Cell, Data
 from clean_isolated import clean_isolated
-from hires_io import parse_i_pairs, write_i_pairs
+from hires_io import parse_i_pairs, write_pairs, write_i_pairs
 def cli(args):
     file_name, out_name1, out_name2, num_thread, up_dense, up_distance = \
-        args.input_file, args.output1, args.output2, args.num_thread, args.dense, args.distance
+        args.input_file, args.output_file1, args.output_file2, int(args.num_thread), int(args.dense), int(args.distance)
     cell = parse_i_pairs(file_name)
     cello1, cello2 = sep_clean(cell, num_thread, up_dense, up_distance)
-    write_i_pairs(cello1)
-    write_i_pairs(cello2)
+    write_pairs(cello1, out_name1)
+    write_i_pairs(cello2, out_name2)
 
 # --------- working module --------
 hap_word = {"1":"(mat)", "0":"(pat)"} #1 for maternal, 0 for paternal
@@ -20,7 +20,9 @@ def add_ap(data:pd.DataFrame, row_picker:pd.Series, col_picker:list, ap:str):
     data.loc[row_picker, col_picker].astype("string")
     data.loc[row_picker, col_picker] = data.loc[row_picker, col_picker].str.cat(ap)
 def rm_hap(data:pd.DataFrame):
-    pass
+    data["chr1"] = data["chr1"].astype("string").str.extract(r'(chr[\dXY]+)')
+    data["chr2"] = data["chr2"].astype("string").str.extract(r'(chr[\dXY]+)')
+    return data
 def sep_clean(cell: Cell, num_thread, up_dense, up_distance) -> Cell:
     # sep pairs haplotype and do clean isolated again
     # generate .hap.pairs for 2D analysis as well as .pairs for hickit 3d building
@@ -37,5 +39,7 @@ def sep_clean(cell: Cell, num_thread, up_dense, up_distance) -> Cell:
         new_frame = pd.concat([new_frame, frame.loc[row_picker, :]])
     cell.get_data("pairs").content = new_frame # cell seperated
     cell_c = clean_isolated(cell, num_thread, up_dense, up_distance) # cell cleaned
+    cell_cf = Cell(name, Data(dtype="pairs", head=head, content=cell_c.get_data("pairs").content["readID chr1 pos1 chr2 pos2 strand1 strand2".split()])) # filter out redundant columns 
     cell_cm = Cell(name, Data(dtype="i_pairs", head=head, content=rm_hap(cell_c.get_data("pairs").content)) ) # cell mingled
-    return cell_c, cell_cm
+    
+    return cell_cf, cell_cm
