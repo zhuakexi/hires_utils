8648a3137f3c3727d5e99474b722d3a8f7307c58
zhuakexi <zhuakexi@126.com>
2021-01-25 14:33:36 +0800

    don't need booter

--- booter.py
+++
@@ -1,8 +0,0 @@
-from hires_io import parse_pairs, write_pairs
-from classes import Cell
-
-def single(handle:"one cell argument", filename:str, outname:str)->int:
-    #call handle once
-    cell = parse_pairs(filename)
-    res = handle(cell)
-    write_pairs(res, outname)
