62130299c99c8b4539c3a2fa8a12a251ee0593c1
zhuakexi <zhuakexi@126.com>
2021-01-26 20:58:05 +0800

    remove class

--- sep_clean.py
+++ sep_clean.py
@@ -1,14 +1,14 @@
 import pandas as pd
-from classes import Cell, Data
-#from clean_isolated import clean_isolated
-#from hires_io import parse_i_pairs, write_pairs, write_i_pairs
+
+from clean_isolated import clean_isolated
+from hires_io import parse_pairs, write_pairs
 def cli(args):
     file_name, out_name1, out_name2, num_thread, up_dense, up_distance = \
-        args.input_file, args.output_file1, args.output_file2, int(args.num_thread), int(args.dense), int(args.distance)
-    cell = parse_i_pairs(file_name)
-    cello1, cello2 = sep_clean(cell, num_thread, up_dense, up_distance)
-    write_pairs(cello1, out_name1)
-    write_i_pairs(cello2, out_name2)
+        args.filename[0], args.output_file1, args.output_file2, int(args.num_thread), int(args.dense), int(args.distance)
+    pairs = parse_pairs(file_name)
+    dip_frame, hickit_frame = sep_clean(pairs, num_thread, up_dense, up_distance)
+    write_pairs(dip_frame, out_name1)
+    write_pairs(hickit_frame, out_name2)
 
 # --------- working module --------
 hap_word = {"1":"(mat)", "0":"(pat)"} #1 for maternal, 0 for paternal
@@ -23,23 +23,26 @@ def rm_hap(data:pd.DataFrame):
     data["chr1"] = data["chr1"].astype("string").str.extract(r'(chr[\dXY]+)')
     data["chr2"] = data["chr2"].astype("string").str.extract(r'(chr[\dXY]+)')
     return data
-def sep_clean(cell: Cell, num_thread, up_dense, up_distance) -> Cell:
+def sep_clean(pairs: pd.DataFrame, num_thread:int, up_dense:int, up_distance:int) -> tuple:
     # sep pairs haplotype and do clean isolated again
-    # generate .hap.pairs for 2D analysis as well as .pairs for hickit 3d building
-    name, data = cell.name, cell.get_data("pairs")
-    head, frame = data.head, data.content
-    new_frame = pd.DataFrame()
+    # generate 9-column .pairs file for 2D analysis as well as longer 13-column .pairs for hickit 3d building
+    # call the former .dip.pairs for simplicity
+    dip_frame = pd.DataFrame()
+    hickit_frame = pd.DataFrame()
     # blank DataFrame can concat any DataFrame
+    # dealing 4 phase_prob columns one by one and stack together
     for code in "00 01 10 11".split():
-        row_picker = frame["phase_prob" + code] >= 0.75
-        frame.loc[row_picker, "phase0"] = code[0]
-        frame.loc[row_picker, "phase1"] = code[1]
-        add_ap(frame, row_picker, "chr1", hap_word[code[0]])
-        add_ap(frame, row_picker, "chr2", hap_word[code[1]])
-        new_frame = pd.concat([new_frame, frame.loc[row_picker, :]])
-    cell.get_data("pairs").content = new_frame # cell seperated
-    cell_c = clean_isolated(cell, num_thread, up_dense, up_distance) # cell cleaned
-    cell_cf = Cell(name, Data(dtype="pairs", head=head, content=cell_c.get_data("pairs").content["readID chr1 pos1 chr2 pos2 strand1 strand2".split()])) # filter out redundant columns 
-    cell_cm = Cell(name, Data(dtype="i_pairs", head=head, content=rm_hap(cell_c.get_data("pairs").content)) ) # cell mingled
-    
-    return cell_cf, cell_cm
+        # pick phased lines(one of phase_probs >= 0.75)
+        row_picker = pairs["phase_prob" + code] >= 0.75
+        # set phase0 and phas1 column according to phase_probs
+        pairs.loc[row_picker, "phase0"] = code[0]
+        pairs.loc[row_picker, "phase1"] = code[1]
+        hickit_frame = pd.concat([hickit_frame, pairs.loc[row_picker, :]])
+        # set chr1 and chr2 column according to phase_probs, hickit_frame don't need this
+        add_ap(pairs, row_picker, "chr1", hap_word[code[0]])
+        add_ap(pairs, row_picker, "chr2", hap_word[code[1]])
+        dip_frame = pd.concat([dip_frame, pairs.loc[row_picker, :]])
+    dip_frame = clean_isolated(dip_frame, num_thread, up_dense, up_distance) # clean isolated contacts for newly generated chromosomes
+    hickit_frame = clean_isolated(hickit_frame, num_thread, up_dense, up_distance)
+    dip_frame.attrs = hickit_frame.attrs = pairs.attrs
+    return dip_frame, hickit_frame
